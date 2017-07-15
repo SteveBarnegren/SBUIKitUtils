28c2a9220053390163aa1244db7e13bac8479aa0
Steve Barnegren <steve.barnegren@gmail.com>
2017-07-15 12:49:18 +0100

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-SBUIKitUtils_Tests/Pods-SBUIKitUtils_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SBUIKitUtils_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SBUIKitUtils_Tests
+@end
---
+++ Example/Pods/Target Support Files/SBUIKitUtils/SBUIKitUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SBUIKitUtils : NSObject
+@end
+@implementation PodsDummy_SBUIKitUtils
+@end
